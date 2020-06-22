e312e45260200e6ce6ec3ecbb037bcd397ad9a45
Clément Sibille <clement.sibille@protonmail.com>
2020-06-22 20:38:50 +0200

    Add SystemSchedule::new() and SystemSchedule::add_system()

--- src/system.rs
+++ src/system.rs
@@ -37,6 +37,15 @@ pub struct SystemSchedule {
 }
 
 impl SystemSchedule {
+    pub fn new() -> Self {
+        Self { systems: vec![] }
+    }
+
+    /// Adds a system to the schedule
+    pub fn add_system<Q: 'static + for<'a> Queryable<'a>>(&mut self, system: System<'static, Q>) {
+        self.systems.push(Box::new(system))
+    }
+
     /// Runs the schedule
     pub fn run(&mut self, ecs: &mut Ecs) {
         for system in &mut self.systems {
@@ -145,6 +154,48 @@ mod tests {
         let mut system_schedule = SystemSchedule {
             systems: vec![Box::new(heal_system), Box::new(teleport_to_origin)],
         };
+
+        system_schedule.run(&mut ecs);
+
+        for (position, health) in <(Imm<Position>, Imm<Health>)>::fetch(&mut ecs) {
+            assert_eq!(position.x, 0.0);
+            assert_eq!(position.y, 0.0);
+            assert_eq!(health.health, 100.0);
+        }
+    }
+
+    #[test]
+    pub fn ecs_system_schedule_new() {
+        let mut ecs = Ecs::new();
+
+        ecs.new_entity()
+            .with_component(Position { x: 0.5, y: 2.3 })
+            .with_component(Speed { x: 1.0, y: 4.0 })
+            .build();
+
+        ecs.new_entity()
+            .with_component(Position { x: 1.0, y: 2.3 })
+            .with_component(Speed { x: 12.0, y: 42.0 })
+            .with_component(Health { health: 100.0 })
+            .with_component(Burnable)
+            .build();
+
+        ecs.new_entity()
+            .with_component(Position { x: 18.2, y: 4.5 })
+            .with_component(Speed { x: 122.0, y: 12.0 })
+            .with_component(Health { health: 95.0 })
+            .with_component(Burnable)
+            .build();
+
+        let mut system_schedule = SystemSchedule::new();
+        system_schedule.add_system(System::<(Mut<Health>,)>::new(|(health,)| {
+            health.health = 100.0;
+        }));
+
+        system_schedule.add_system(System::<(Mut<Position>,)>::new(|(position,)| {
+            position.x = 0.0;
+            position.y = 0.0;
+        }));
 
         system_schedule.run(&mut ecs);
 
