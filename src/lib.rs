b517ff4277a94e9188b6bd086617476768e12879
Clément Sibille <claymeuns@protonmail.com>
2021-02-05 07:43:41 +0100

    Start implementing archetype storage
    This implementation is broken for now but that's a start

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::alloc::GlobalAlloc;
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
+use std::ptr::NonNull;
 
 pub type EntityId = usize;
 pub type Component = Box<dyn Any>;
@@ -19,6 +21,7 @@ impl Ecs {
     }
 
     pub fn create_entity<D: ComponentsDefinition>(&mut self, _components_definition: D) {
+        let _entity_id = self.entity_store.allocate_entity();
         let _archetype = self.get_or_insert_archetype::<D>();
     }
 
@@ -33,7 +36,65 @@ impl Ecs {
     fn get_or_insert_archetype<D: ComponentsDefinition>(&mut self) -> &mut Archetype {
         self.archetypes
             .entry(D::component_types())
-            .or_insert(Archetype::new())
+            .or_insert(Archetype::new::<D>())
+    }
+}
+
+pub struct Archetype {
+    types_metadata: Vec<TypeMetadata>,
+    data: NonNull<u8>,
+    entity_count: usize,
+    capacity: usize,
+}
+
+impl Archetype {
+    pub fn new<C: ComponentsDefinition>() -> Self {
+        Self {
+            types_metadata: C::metadata(),
+            data: NonNull::dangling(),
+            entity_count: 0,
+            capacity: 0,
+        }
+    }
+
+    pub fn store_component(&mut self, component_data: *const u8, type_index: usize) {
+        if self.capacity < self.entity_count + 1 {
+            self.grow();
+        }
+
+        unsafe {
+            std::ptr::copy_nonoverlapping(
+                component_data,
+                self.data.as_ptr().offset(self.entity_count as isize),
+                1,
+            )
+        }
+
+        if type_index == 0 {
+            self.entity_count += 1;
+        }
+    }
+
+    fn grow(&mut self) {
+        unsafe {
+            let _entity_alignment = self.types_metadata[0].entity_alignment;
+            let _entity_size = self.types_metadata[0].entity_size;
+            let entity_layout = self.types_metadata[0].entity_layout;
+
+            // TODO handle realloc
+            let (new_capacity, ptr) = {
+                let ptr = std::alloc::System.alloc(entity_layout);
+                (1, ptr)
+            };
+
+            // TODO handle error
+            self.data = NonNull::new_unchecked(ptr);
+            self.capacity = new_capacity;
+        }
+    }
+
+    pub fn entity_count(&self) -> usize {
+        self.entity_count
     }
 }
 
@@ -71,22 +132,46 @@ impl EntityStore {
     }
 }
 
-pub struct Archetype;
-
-impl Archetype {
-    pub fn new() -> Self {
-        Self
-    }
-}
-
 pub trait ComponentsDefinition {
     fn component_types() -> Box<[ComponentType]>;
+    fn metadata() -> Vec<TypeMetadata>;
+    fn store_components(&mut self, archetype: &mut Archetype);
 }
 
 impl<A: 'static, B: 'static> ComponentsDefinition for (A, B) {
     fn component_types() -> Box<[ComponentType]> {
         Box::new([TypeId::of::<A>(), TypeId::of::<B>()])
     }
+
+    fn metadata() -> Vec<TypeMetadata> {
+        let mut metadata = vec![];
+
+        // TODO store only once
+        let entity_size = std::mem::size_of::<(A, B)>();
+        let entity_alignment = std::mem::align_of::<(A, B)>();
+        let entity_layout = std::alloc::Layout::new::<(A, B)>();
+        metadata.push(TypeMetadata {
+            entity_size,
+            entity_alignment,
+            entity_layout,
+        });
+        metadata.push(TypeMetadata {
+            entity_size,
+            entity_alignment,
+            entity_layout,
+        });
+        metadata
+    }
+    fn store_components(&mut self, archetype: &mut Archetype) {
+        archetype.store_component(&self.0 as *const A as *const u8, 0usize);
+        archetype.store_component(&self.1 as *const B as *const u8, 1usize);
+    }
+}
+
+pub struct TypeMetadata {
+    entity_size: usize,
+    entity_alignment: usize,
+    entity_layout: std::alloc::Layout,
 }
 
 #[cfg(test)]
@@ -144,5 +229,19 @@ mod tests {
         let second_entity_id = entity_store.allocate_entity();
         assert_eq!(entity_store.entity_count(), 1);
         assert_eq!(second_entity_id, 1)
+    }
+
+    #[test]
+    pub fn archetype_new() {
+        let archetype = Archetype::new::<(Position, Velocity)>();
+        assert_eq!(archetype.types_metadata.len(), 2);
+    }
+
+    #[test]
+    pub fn archetype_store() {
+        let mut archetype = Archetype::new::<(Position, Velocity)>();
+        (Position { x: 3f32, y: 5f32 }, Velocity { x: 8f32, y: 6f32 })
+            .store_components(&mut archetype);
+        assert_eq!(archetype.entity_count(), 1);
     }
 }
